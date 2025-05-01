2353f22dfa4fa5ed9dac38f57a762aaa387f818f
Gabriel Almeida de carvalho <gabrielac0424@gmail.com>
2025-05-01 15:49:15 -0300

    Correção de templates e CRUD funcional

--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Corrigido o import
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", TodoListView.as_view(), name='todo_list'),
+    path("create/", TodoCreateView.as_view(), name='todo_create'),  # Adicionada barra no final
+    path("update/<int:pk>/", TodoUpdateView.as_view(), name='todo_update'),  # Barra no final
+    path("delete/<int:pk>/", TodoDeleteView.as_view(), name='todo_delete'),  # Barra no final
 ]
+    
+
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView  # Corrigido "vlews" para "views"
+from django.urls import reverse_lazy
+from .models import Todo  # Certifique-se de ter esse modelo criado
 
-# Create your views here.
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):  # Corrigido "createView" para "CreateView"
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+# Na TodoUpdateView (views.py)
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    template_name = 'todos/todo_form.html'  # Caminho corrigido
+    success_url = reverse_lazy('todo_list')
+
+# Na TodoDeleteView (views.py)
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = 'todos/todo_confirm_delete.html'  # Caminho corrigido
+    success_url = reverse_lazy('todo_list')
